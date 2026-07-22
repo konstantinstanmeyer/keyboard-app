daa7f5ddbd05d0debae4abf74bacfd56cab6fc90
KonstantinStanmeyer <konstantin@stanmeyer.com>
2026-07-22 09:00:47 -0400

    functional postman requests

--- mongodb/connection.ts
+++ mongodb/connection.ts
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 const mongoDBConnection = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI as string);
-        // console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB");
     }catch(e){
         console.log(e);
     }
